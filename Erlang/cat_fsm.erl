37c14a7287a428b618e8b3cf55afd8691482537c
Wang Zhijun <madfrogme@gmail.com>
2014-05-01 14:57:42 +0900

    cat just dont give a crap

---
+++ Erlang/cat_fsm.erl
@@ -0,0 +1,28 @@
+-module(cat_fsm).
+-export([start/0, event/2]).
+
+start() ->
+	spawn(fun() -> dont_give_crap() end).
+
+event(Pid, Event) ->
+	% 気づいたことはいつもクライアントがRefをつくる
+	% なぜかというとクライアントからメッセージを送るから
+	Ref = make_ref(),
+	Pid ! {self(), Ref, Event},
+	receive
+		% Msgはサーバのdont_give_crap()から
+		{Ref, Msg} -> 
+			{ok, Msg}
+	after 5000 ->
+		{error, timeout}
+	end.
+
+dont_give_crap() ->
+	receive
+		{Pid, Ref, _Msg} ->
+			Pid ! {Ref, meh};
+		_ -> 
+			ok
+	end,
+	io:format("Switching to 'dont_give_crap' state~n"),
+	dont_give_crap().
