9c075d6fa9757b923efe31b3ce922924d0c3bad2
Wang Zhijun <madfrogme@gmail.com>
2014-05-31 13:19:22 +0900

    add erlang files

--- Erlang/funs.erl
+++ Erlang/funs.erl
@@ -84,3 +84,14 @@ fold(F, Start, [H|T]) ->
 	fold(F, F(H,Start), T).
 
 
+odds_and_evens(L) ->
+	odds_and_evens_acc(L, [],[]).
+
+odds_and_evens_acc([H|T], Odds, Evens) ->
+	case (H rem 2) of
+		1 -> odds_and_evens_acc(T,[H|Evens],Odds );
+		0 -> odds_and_evens_acc(T,Odds, [H|Evens])
+	end;
+odds_and_evens_acc([],Odds, Evens) ->
+	{lists:reverse(Odds), lists:reverse(Evens)}.
+
